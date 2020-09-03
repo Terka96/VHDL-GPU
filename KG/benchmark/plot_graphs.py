4cc829e89da9bc2fc8c3f595fe5c5ba30e810881
Terka96 <piotr.terczynski1@gmail.com>
2020-09-03 02:39:37 +0200

    metrics and report generator

---
+++ KG/benchmark/plot_graphs.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+#params
+cu = 4
+inst = 144 #143 + 1
+oper = 16
+
+import matplotlib.pyplot as plt
+import sys
+import math
+import statistics
+
+workingdir = sys.argv[1]
+
+count_instruction = [0 for i in range(inst)]
+count_operation = [0 for i in range(oper)]
+cycles_instruction = [0 for i in range(inst)]
+cycles_operation = [0 for i in range(oper)]
+generated_pixels = []
+drawn_pixels = []
+prepared_texels = []	#w sumie to teraz powinno być to samo co generated pixels
+fb_utilization = []
+cu_utilization = [[] for i in range(cu)]
+cycles_cu_waiting_for_pixel_poll = [[] for i in range(cu)]
+cycles_cu_waiting_for_texel = [[] for i in range(cu)]
+timeline = []
+
+#read the data
+cyclesinwindow = 25000
+ts = open(sys.argv[1]+"time_series","r")
+line = ts.readline()
+timesample = 0
+while (line != ''):
+	splitted = line.split(" ")
+	timeline += [int((timesample+1)/2)]
+	generated_pixels += [int(splitted[2])]
+	drawn_pixels += [int(splitted[3])]
+	prepared_texels += [int(splitted[4])]	
+	fb_utilization += [int(splitted[5])/cyclesinwindow]
+	for i in range(4):
+		line = ts.readline()
+		cusplit = line.split(" ")
+		cu_utilization[i] += [(int(cusplit[1])-int(cusplit[2])-int(cusplit[3]))/cyclesinwindow]
+		cycles_cu_waiting_for_pixel_poll[i] += [int(cusplit[2])]
+		cycles_cu_waiting_for_texel[i] += [int(cusplit[3])]
+		offset=4
+		for j in range(offset,offset+inst):
+			cycles_instruction[j-offset] += int(cusplit[j])	
+		offset += inst
+		for j in range(offset,offset+oper):
+			cycles_operation[j-offset] += int(cusplit[j])	
+		offset += oper
+		for j in range(offset,offset+inst):
+			count_instruction[j-offset] += int(cusplit[j])
+		offset += inst
+		for j in range(offset,offset+oper):
+			count_operation[j-offset] += int(cusplit[j])
+	timesample += 1
+	line = ts.readline()
+	
+ts.close()
+
+#too much samples group them!
+group=50 #1 sample will be 25ms = 1250000cycles
+grouped_cuu = []
+grouped_gp = [sum(generated_pixels[i:i+group]) for i in range(0, len(generated_pixels), group)]   
+grouped_dp = [sum(drawn_pixels[i:i+group]) for i in range(0, len(drawn_pixels), group)]   
+grouped_pt = [sum(prepared_texels[i:i+group]) for i in range(0, len(prepared_texels), group)]
+grouped_fbu = [statistics.mean(fb_utilization[i:i+group]) for i in range(0, len(fb_utilization), group)]
+for j in range(cu):
+	grouped_cuu.insert(j,[statistics.mean(cu_utilization[j][i:i+group]) for i in range(0, len(cu_utilization[j]), group)])
+timeline = range(0,timesample, group)
+
+#plot
+plt.bar(range(inst),[x/1000000 for x in count_instruction], color = 'blue')
+plt.savefig(workingdir+"count_instructions.png",dpi=3000) 
+plt.close()
+plt.bar(range(inst),[x/1000000 for x in cycles_instruction], color = 'green')
+plt.savefig(workingdir+"cycles_instructions.png",dpi=3000)
+plt.close()
+plt.bar(range(oper),[x/1000000 for x in count_operation], color = 'blue')
+plt.savefig(workingdir+"count_operations.png",dpi=3000)
+plt.close()
+plt.bar(range(oper),[x/1000000 for x in cycles_operation], color = 'green')
+plt.savefig(workingdir+"cycles_operations.png",dpi=3000)
+plt.close()
+
+
+plt.plot(timeline,grouped_gp, color = 'green')
+plt.plot(timeline,grouped_dp, color = 'blue')
+plt.plot(timeline,grouped_pt, color = 'red')
+plt.savefig(workingdir+"pixels.png",dpi=3000)
+plt.close()
+
+
+colors=['red','green','blue','orange','pink','yellow']
+plt.plot(timeline,grouped_fbu, color = 'purple')
+for i in range(cu):
+	plt.plot(timeline,grouped_cuu[i], color = colors[i])
+plt.savefig(workingdir+"utilization.png",dpi=3000)
+plt.close()
+
+stat = open(workingdir+"stat.txt","w")
+stat.write("sum_cycles:"+str(sum(cycles_instruction))+"\n")
+stat.write("sum_executed_instrucitons:"+str(sum(count_operation))+"\n")
+for i in range(cu):
+	stat.write("CU"+str(i+1)+" cycles_cu_waiting_for_pixel_poll="+str(sum(cycles_cu_waiting_for_pixel_poll[i]))+"\n")
+	stat.write("CU"+str(i+1)+" cycles_cu_waiting_for_texel="+str(sum(cycles_cu_waiting_for_texel[i]))+"\n")
+stat.close()
---
+++ KG/metrics.vhd
@@ -0,0 +1,125 @@
+----------------------------------------------------------------------------------
+-- METRICS
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use std.textio.all;
+use work.definitions.all;
+
+entity metrics is
+	port(
+		clk : in std_logic;	--system clock
+		cu_rd : in std_logic_vector(1 to CU_COUNT);
+		cu_ce : in std_logic_vector(1 to CU_COUNT);
+		fb_rd : in std_logic;
+		cu_pc_data : in std_logic_vector(1 to CU_COUNT);
+		pc_fb_data : in std_logic;
+		cu_tex_load_en : in std_logic_vector(1 to CU_COUNT);
+		cu_tex_rd : in std_logic_vector(1 to CU_COUNT);
+		operation : in CU_INTEGERS;
+		instruction_number : in CU_INTEGERS;
+		pixel_drawn : in std_logic
+	);
+end metrics;
+
+architecture Behavioral of metrics is
+type instruction_distribution is array(0 to END_PROGRAMME) of integer;
+type operation_distribution is array(0 to 15) of integer;
+type cu_instruction_distribution is array(1 to CU_COUNT) of instruction_distribution;
+type cu_operation_distribution is array(1 to CU_COUNT) of operation_distribution;
+begin
+
+time_series : process (clk,operation,instruction_number) is
+	file file_pointer: text open write_mode is "run_logs/time_series";
+	variable l : line;
+	variable space : string(1 to 1) := " ";
+	
+	
+	variable generated_pixels : integer := 0;
+	variable drawn_pixels : integer := 0;
+	variable cycles_instruction : cu_instruction_distribution := (others => (others => 0));
+	variable cycles_operation : cu_operation_distribution := (others => (others => 0));
+	variable count_instruction : cu_instruction_distribution := (others => (others => 0));
+	variable count_operation : cu_operation_distribution := (others => (others => 0));
+	variable last_operations : CU_INTEGERS := (others => 0);
+	variable last_instructions : CU_INTEGERS := (others => 0);
+	variable cu_utilization : CU_INTEGERS := (others => 25000);
+	variable fb_utilization : integer :=25000;
+	variable prepared_texels : integer := 0;
+	variable cycles_cu_waiting_for_pixel_poll : CU_INTEGERS := (others => 0);
+	variable cycles_cu_waiting_for_texel : CU_INTEGERS := (others => 0);
+	variable cycles : integer := 0;
+begin
+	--data collection
+	if rising_edge(clk) then
+		cycles := cycles + 1;
+		for i in 1 to CU_COUNT loop
+			cycles_instruction(i)(instruction_number(i)) := cycles_instruction(i)(instruction_number(i)) + 1;
+			cycles_operation(i)(operation(i)) := cycles_operation(i)(operation(i)) + 1;
+			if last_operations(i) /= operation(i) then last_operations(i) := operation(i); count_operation(i)(operation(i)) := count_operation(i)(operation(i)) + 1; end if;
+			if last_instructions(i) /= instruction_number(i) then last_instructions(i) := instruction_number(i); count_instruction(i)(instruction_number(i)) := count_instruction(i)(instruction_number(i)) + 1; end if;
+			if cu_pc_data(i) = '1' then cycles_cu_waiting_for_pixel_poll(i) := cycles_cu_waiting_for_pixel_poll(i) + 1; end if;
+			if cu_tex_load_en(i) = '1' then cycles_cu_waiting_for_texel(i) := cycles_cu_waiting_for_texel(i) + 1; end if;
+			if cu_rd(i) = '1' then cu_utilization(i) := cu_utilization(i) - 1; end if;
+			if cu_tex_rd(i) = '1' then prepared_texels := prepared_texels + 1; end if;	--tylko jeden na raz moze byc rd
+		end loop;
+		if fb_rd = '1' then fb_utilization := fb_utilization - 1; end if;
+		if pixel_drawn = '1' then drawn_pixels := drawn_pixels + 1; end if;
+		if pc_fb_data = '1' then generated_pixels := generated_pixels + 1; end if;
+	end if;
+	
+	--output
+	if cycles >= 25000 then --with 50MHz clk it is 0.5ms
+		write(l,now);
+		write(l,space);
+		write(l,integer'image(generated_pixels));
+		write(l,space);
+		write(l,integer'image(drawn_pixels));
+		write(l,space);
+		write(l,integer'image(prepared_texels));
+		write(l,space);
+		write(l,integer'image(fb_utilization));
+		writeline(file_pointer,l);
+		for i in 1 to CU_COUNT loop
+			write(l,integer'image(i));
+			write(l,space);
+			write(l,integer'image(cu_utilization(i)));
+			write(l,space);
+			write(l,integer'image(cycles_cu_waiting_for_pixel_poll(i)));
+			write(l,space);
+			write(l,integer'image(cycles_cu_waiting_for_texel(i)));
+			write(l,space);
+			for j in 0 to END_PROGRAMME loop
+				write(l,integer'image(cycles_instruction(i)(j)));
+				write(l,space);
+			end loop;
+			for j in 0 to 15 loop
+				write(l,integer'image(cycles_operation(i)(j)));
+				write(l,space);
+			end loop;
+			for j in 0 to END_PROGRAMME loop
+				write(l,integer'image(count_instruction(i)(j)));
+				write(l,space);
+			end loop;
+			for j in 0 to 15 loop
+				write(l,integer'image(count_operation(i)(j)));
+				write(l,space);
+			end loop;
+			writeline(file_pointer,l);
+		end loop;
+		cycles := 0;
+		generated_pixels := 0;
+		drawn_pixels := 0;
+		cycles_instruction := (others => (others => 0));
+		cycles_operation := (others =>(others => 0));
+		cu_utilization := (others => 25000);
+		fb_utilization := 25000;
+		prepared_texels := 0;
+		cycles_cu_waiting_for_pixel_poll := (others => 0);
+		cycles_cu_waiting_for_texel := (others => 0);
+	end if;
+end process;
+
+
+end Behavioral;
+
